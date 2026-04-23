6e29459206286459d72a3eae951ccdbd661d2ba2
Bryan Hoyem <bjhoyem@gmail.com>
2026-04-23 16:30:27 -0500

    Working on the activities page. Created basic page divisions and getting the calendar looking right.

--- client/src/components/Activity.tsx
+++ client/src/components/Activity.tsx
@@ -1,7 +1,44 @@
-const Activity = () => {
+import Calendar from "react-calendar";
+// import "react-calendar/dist/Calendar.css";
+// import { DayPicker } from "react-day-picker";
+// import "react-day-picker/dist/style.css";
+// import "./calendar.css";
+
+export function MyCalendar() {
   return (
-    <h2>Activity</h2>
+    <div className="flex justify-center w-full p-4 outline-blue-500 outline-solid outline-2">
+      <Calendar calendarType="gregory"/>
+    </div>
+  );
+}
+
+export function MyDayPicker() {
+  return (
+    <div className = "p-4 outline-blue-500 outline-solid outline-2 ">
+      <DayPicker />
+    </div>
   )
 }
 
-export default Activity
+const Activity = () => {
+  return (
+    <>
+      <div className="page-container flex justify-center w-full gap-4">
+        <div className="activities-list w-[60%] ml-[40px] mr-[16px] my-[40px] outline-red-500 outline-solid outline-2">
+          <h3>list of activities</h3>
+        </div>
+        <div className="right-side-tiles w-[40%] my-[40px] ml-[16px] mr-[40px] flex flex-col gap-3 outlinel-green-500 outline-solid outline-2">
+          <MyCalendar />
+          <h3 className="m-2 outline-blue-500 outline-solid outline-2">
+            streak counter
+          </h3>
+          <h3 className="m-2 outline-blue-500 outline-solid outline-2">
+            today's summary
+          </h3>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Activity;
